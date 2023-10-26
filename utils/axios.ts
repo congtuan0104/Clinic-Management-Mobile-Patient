413854de1ee4d1b106a6917215ec7052b0a8a8aa
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-10-26 15:36:59 +0700

    Configuring axios

---
+++ utils/axios.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+
+export const REQUEST_TIMEOUT = 30000;
+
+export const axiosClient = axios.create({
+  // host api được cấu hình trong vite.config.ts -> thay đổi theo env
+  baseURL: "http://localhost:2222/api",
+  timeout: REQUEST_TIMEOUT,
+});
