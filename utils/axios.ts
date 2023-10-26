9e0fee490c4c849089ad16e682edb67961f8bf02
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-10-26 15:54:28 +0700

    Add comment about config axios

--- utils/axios.ts
+++ utils/axios.ts
@@ -6,4 +6,9 @@ export const axiosClient = axios.create({
   // host api được cấu hình trong vite.config.ts -> thay đổi theo env
   baseURL: "http://localhost:2222/api",
   timeout: REQUEST_TIMEOUT,
+
+  // Config header token
+  // headers: {
+  //   Authorization: 'Bearer token'
+  // }
 });
