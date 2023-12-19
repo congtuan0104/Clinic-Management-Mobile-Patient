de745f005d5d6abf8f5c7edd919ff663aca915ab
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-12-19 21:04:44 +0700

    Connect notification with realtime DB

--- screens/NotificationScreen/NotificationListScreen.tsx
+++ screens/NotificationScreen/NotificationListScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Text,
   Box,
@@ -10,44 +10,102 @@ import {
 } from "native-base";
 import { NotificationListScreenProps } from "../../Navigator/NotificationNavigator";
 import NotificationItem from "../../components/NotificationItem/NotificationItem";
+import { useAppSelector } from "../../hooks";
+import { userInfoSelector } from "../../store";
+import { firebase } from "@react-native-firebase/database";
+import { Notification } from "../../types/notification.types";
+import dayjs from "dayjs";
 
 const NotificationListScreen = ({
   navigation,
   route,
 }: NotificationListScreenProps) => {
+  // using useEffect to get all notification on realtime database
+  const userInfo = useAppSelector(userInfoSelector);
+  const [notificationList, setNotificationList] = useState<Notification[]>([]);
+  const [initialNotificationList, setInitialNotificationList] = useState<
+    Notification[]
+  >([]);
+  useEffect(() => {
+    setNotificationList([]);
+    setInitialNotificationList([]);
+    const reference = firebase
+      .app()
+      .database(
+        "https://clinus-1d1d1-default-rtdb.asia-southeast1.firebasedatabase.app/"
+      )
+      .ref(`/notifications/${userInfo?.id}`);
+    const onChildAdd = reference.on("child_added", (snapshot) => {
+      console.log(snapshot.val());
+      setNotificationList((state) => [snapshot.val(), ...state]);
+      setInitialNotificationList((state) => [snapshot.val(), ...state]);
+    });
+    // Stop listening for updates when no longer required
+    return () => reference.off("child_added", onChildAdd);
+  }, [userInfo?.id]);
+
+  const handleReadNotification = (id: string) => {
+    const updatedNotificationList = notificationList.map((notification) => {
+      if (notification.id === id) {
+        return {
+          ...notification,
+          isRead: true,
+        };
+      }
+      return notification;
+    });
+    setInitialNotificationList(updatedNotificationList);
+    setNotificationList(updatedNotificationList);
+  };
+
+  const showUnreadNotification = () => {
+    const unreadNotificationList = notificationList.filter(
+      (notification) => notification.isRead === false
+    );
+    setNotificationList(unreadNotificationList);
+  };
+  const showAllNotification = () => {
+    setNotificationList(initialNotificationList);
+  };
   return (
     <Box
       backgroundColor="#fff"
       borderRadius={20}
+      minW="90%"
       maxW="90%"
       alignSelf="center"
       p={5}
       maxH="95%"
     >
       <HStack space={5} mb={5}>
-        <Button borderRadius={20}>Tất cả</Button>
-        <Button borderRadius={20}>Chưa đọc</Button>
+        <Button onPress={showAllNotification} borderRadius={20}>
+          Tất cả
+        </Button>
+        <Button onPress={showUnreadNotification} borderRadius={20}>
+          Chưa đọc
+        </Button>
       </HStack>
       <ScrollView>
         <VStack space={5}>
-          <NotificationItem
-            image="https://picsum.photos/200"
-            content="Đây là thông báo 1"
-            time="12/12/2023, 14:44:44"
-            isRead={true}
-          />
-          <NotificationItem
-            image="https://picsum.photos/200"
-            content="Đây là thông báo 2"
-            time="12/12/2023, 14:44:44"
-            isRead={false}
-          />
-          <NotificationItem
-            image="https://picsum.photos/200"
-            content="Đây là thông báo 3"
-            time="12/12/2023, 14:44:44"
-            isRead={true}
-          />
+          {notificationList?.length ? (
+            notificationList.map((notification) => {
+              return (
+                <NotificationItem
+                  key={notification.id}
+                  id={notification.id}
+                  image="https://picsum.photos/200"
+                  content={notification.content}
+                  time={dayjs(notification.sendingTime).format(
+                    "DD/MM/YYYY HH:mm:ss"
+                  )}
+                  isRead={notification.isRead}
+                  handleReadNotification={handleReadNotification}
+                />
+              );
+            })
+          ) : (
+            <Text>Bạn chưa có thông báo nào</Text>
+          )}
         </VStack>
       </ScrollView>
     </Box>
